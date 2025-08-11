b57e7ac87cb0b51cea6210b2a39ee04400d9e02b
josephadogeriopao <joseph.adogeri@orleansassessors.com>
2025-08-11 13:25:35 -0500

    Implemented entity ' User'

--- app/src/entities/User.ts
+++ app/src/entities/User.ts
@@ -1,3 +1,41 @@
-export class User{
-    constructor(){}
+import { Entity, PrimaryGeneratedColumn, Column,CreateDateColumn, UpdateDateColumn } from "typeorm";
+
+@Entity()
+export class User {
+    @PrimaryGeneratedColumn()
+    id: number;
+
+    @Column({type: "varchar", length: 40, unique: true,})
+    username: string;
+
+    @Column({type: "varchar", length: 40, unique: true,})
+    email: string;
+
+    @Column({type: "varchar", length: 20})
+    password: string;
+
+    @Column({type: "varchar", length: 20})
+    phone: string;
+
+    @Column({ default: true, type:"boolean" })
+    isEnabled: boolean;
+
+    @Column({ type: "int", default: 0 })
+    failedLogins: number;
+
+    @CreateDateColumn()
+    createdAt!: Date; // This column will automatically store the creation date
+
+    @UpdateDateColumn()
+    updatedAt!: Date; // This column will automatically store the last update date
+
+    constructor(id :number, username : string, email : string, password : string, phone: string ){
+        this.id = id;
+        this.username = username;
+        this.email = email;
+        this.password = password;
+        this.phone = phone;
+        this.isEnabled = true;
+        this.failedLogins = 0;
+    }
 }
