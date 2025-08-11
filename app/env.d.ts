41dda4cf36dc4fa3d7bbf8ed4a34be5146bd7999
josephadogeriopao <joseph.adogeri@orleansassessors.com>
2025-08-11 11:39:37 -0500

    Added env.d.ts template

---
+++ app/env.d.ts
@@ -0,0 +1,31 @@
+declare global {
+    namespace Express {
+      interface Request {
+        user: {
+            username:string;
+            email:string
+        },
+        // body: IUser | IUserReset | IUserDeactivated | IUserForgot | IUserAuthorized
+      }
+    }
+    namespace NodeJS {
+      interface ProcessEnv {
+        NODE_ENV: string;
+        DATABASE: string;
+        JSON_WEB_TOKEN_SECRET: string;
+        BCRYPT_SALT_ROUNDS : string;
+        BASE_URL : string;
+        NANOID_SIZE : string;
+        PORT: string;
+        NODEMAILER_USERNAME : string;
+        NODEMAILER_PASSWORD : string;
+        TWILIO_ACCOUNT_SID : string;
+        TWILIO_AUTH_TOKEN : string;
+        TWILIO_PHONE_NUMBER : string;
+        COMPANY : string;
+      }
+    }
+  }
+
+  export {}
+
--- app/src/data-source.ts
+++ app/src/data-source.ts
@@ -4,7 +4,7 @@ import { DataSource } from "typeorm";
 
 export const AppDataSource = new DataSource({
     type: "sqlite",
-    database: "database.sqlite", // Name of your SQLite database file
+    database: process.env., // Name of your SQLite database file
     synchronize: true, // Automatically create/update tables based on entities (for development)
     logging: false, // Set to true for detailed SQL logging
     entities: [], // List your entities here
