71c5a3aa74f6134d60d018df8c21f9e159a509f0
josephadogeriopao <joseph.adogeri@orleansassessors.com>
2025-08-11 11:23:47 -0500

    Added code template to start API server

---
+++ app/index.ts
@@ -0,0 +1,21 @@
+
+import { AppDataSource } from "./src/data-source";
+import express, { Response, Request } from "express";
+
+import bodyParser from "body-parser";
+
+AppDataSource.initialize()
+    .then(async () => {
+        const app = express();
+        app.use(express.json());
+
+        app.use(bodyParser.json())
+        app.get('/', (req: Request, res : Response) => {
+        res.send({message:"Welcome to Server API"});
+        });
+                app.listen(3500, () => {
+            console.log("Server running on port 3500");
+        });
+    })
+    .catch((error) => console.log(error));
+    
