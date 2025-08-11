134c050f2467b5db28a05ad432b44f11eb3af49f
josephadogeriopao <joseph.adogeri@orleansassessors.com>
2025-08-11 13:42:59 -0500

    Implemented entity 'Auth'

--- app/src/entities/Auth.ts
+++ app/src/entities/Auth.ts
@@ -1,3 +1,18 @@
-export class Auth{
-    constructor(){}
+import { Entity, PrimaryGeneratedColumn, Column,OneToOne, JoinColumn } from "typeorm";
+import { User } from "./User";
+
+@Entity()
+export class Auth {
+    @PrimaryGeneratedColumn()
+    id: number;
+
+    @OneToOne(()=> User)
+    @JoinColumn() 
+    user: User;
+
+    constructor(id :number, user: User){
+        this.id = id;
+        this.user = user;
+    }
+
 }
