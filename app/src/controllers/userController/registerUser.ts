633064d5e019a80a40e7307533e382dbf7968162
josephadogeriopao <joseph.adogeri@orleansassessors.com>
2025-08-12 16:08:15 -0500

    Modified error response

--- app/src/controllers/userController/registerUser.ts
+++ app/src/controllers/userController/registerUser.ts
@@ -53,8 +53,15 @@ try{
       // You can further inspect error.driverError for SQLite-specific details if available
       // For example, SQLite unique constraint violation might have a specific code or message
       if (error.message.includes("SQLITE_CONSTRAINT")) {
-          console.error("Unique constraint violation detected.");
-      }
+          //console.error("Unique constraint violation detected.");
+          // console.log("error.name :", error.name);
+          // console.log("error.cause : " , error.cause);
+          // console.log("error.stack : ", error.stack);
+          // console.log("error : ", error.driverError);
+          // console.log("error.message : ", error.message)
+          errorBroadcaster(res, 500, error.message);
+          //res.status(500).json({"message": error.message})
+      }      
     }else if (error instanceof Response) {
       // If the error is a Response object (from `throw response` above)
       error.text().then(errorMessage => {
