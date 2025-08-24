dc6271b99eeb282e68c137740e8b7f1609ef55b1
joseph adogeri <70236136+jadogeri@users.noreply.github.com>
2025-08-24 16:34:25 -0500

    Added JSDoc to index.ts

--- app/src/controllers/itemController/index.ts
+++ app/src/controllers/itemController/index.ts
@@ -1,3 +1,10 @@
+/**
+ * @author Joseph Adogeri
+ * @version 1.0
+ * @since 24-AUG-2025
+ * @description Single export for all item controller functions
+ */
+
 import { addItem } from "./addItem";
 import { deleteItem } from "./deleteItem";
 import { deleteItems } from "./deleteItems";
