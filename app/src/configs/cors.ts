2426d80592e0c4f012c90a53fbd91f779f1f3b3c
joseph adogeri <70236136+jadogeri@users.noreply.github.com>
2025-08-24 15:47:40 -0500

    Updated JSDoc for cors configuration

--- app/src/configs/cors.ts
+++ app/src/configs/cors.ts
@@ -1,3 +1,11 @@
+/**
+ * @author      Joseph Adogeri
+ * @since       24-AUG-2025
+ * @version     1.0
+ * @description configuration setting for cors
+ *  
+ */
+
 export const corsOptions = {
     origin:'*', 
     credentials:true,            //access-control-allow-credentials:true
