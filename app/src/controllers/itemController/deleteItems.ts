3fd716dcd5e9a279b783906eecca77e12978b2a0
joseph adogeri <70236136+jadogeri@users.noreply.github.com>
2025-08-24 16:19:51 -0500

    Added JSDoc in deleteItems.ts

--- app/src/controllers/itemController/deleteItems.ts
+++ app/src/controllers/itemController/deleteItems.ts
@@ -2,18 +2,16 @@
  * @author Joseph Adogeri
  * @version 1.0
  * @since 11-AUG-2025
- *
+ * @description function to delete all items by user in database
  */
 
 const asyncHandler = require("express-async-handler");
 import { Response } from 'express';
 import { IJwtPayload } from '../../interfaces/IJWTPayload';
 import * as itemService from"../../services/itemService"
-import * as userService from"../../services/userService"
 import { Item } from '../../entities/Item';
 import exceptionHandler from '../../utils/exceptionHandler';
 import { errorBroadcaster } from '../../utils/errorBroadcaster';
-import { User } from '../../entities/User';
 
 /**
 *@desc delete an item
@@ -23,20 +21,22 @@ import { User } from '../../entities/User';
 
 export const deleteItems = asyncHandler(async (req : IJwtPayload, res: Response) => {
 
-    if(req.user){
-    const items : Item[] = await itemService.getAll(req);
-    if(items.length === 0){
-      res.status(200).json({ message: `all items of user id : ${req.user.id} already empty` });
+  if(req.user){
+    try{
+      const items : Item[] = await itemService.getAll(req);
+      if(items.length === 0){
+        res.status(200).json({ message: `all items of user id : ${req.user.id} already empty` });
+      }      
+      await itemService.removeAll(req)
+      .then(()=>{
+        res.status(200).json({ message: `deleted all items of user id : ${req.user.id}` });
+      });
+    }catch(error: unknown){
+      exceptionHandler(error, errorBroadcaster, res) 
     }
-      
-    await itemService.removeAll(req)
-    .then(()=>{
-      res.status(200).json({ message: `deleted all items of user id : ${req.user.id}` });
-
-    });
   }
   else{
-  res.status(400).json({ message: "Invalid User" });
+    res.status(400).json({ message: "Invalid User" });
   }
 
 });
