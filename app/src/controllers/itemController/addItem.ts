11ede683603a56dbbbddb6bbe76a9f3d4bfe4ea2
joseph adogeri <70236136+jadogeri@users.noreply.github.com>
2025-08-24 16:02:45 -0500

    fixed bugs in validateTokenHandler.ts

--- app/src/controllers/itemController/addItem.ts
+++ app/src/controllers/itemController/addItem.ts
@@ -2,36 +2,32 @@
  * @author Joseph Adogeri
  * @version 1.0
  * @since 11-AUG-2025
- *
+ * @description function to add an item to database
  */
 
 const asyncHandler = require("express-async-handler");
-import { Response, Request } from 'express';
+import { Response } from 'express';
 import { IItem } from '../../interfaces/IItem';
 import * as ItemService from  "../../services/itemService"
 import { IItemCreateRequest } from "../../interfaces/IItemCreateRequest";
 import { errorBroadcaster } from '../../utils/errorBroadcaster';
-
+import exceptionHandler from '../../utils/exceptionHandler';
 
 /**
-*@desc Add Auth Token
-*@route POST /api/Item/create
+*@desc Add an Item
+*@route POST /api/Items/
 *@access public
 */
 
 export const addItem = asyncHandler(async (req: IItemCreateRequest, res : Response)  => {
 
   const { name } = req.body;
-if(req){
-  console.log("user data === ",name)
-
- if (!name ) {
-    res.status(400);
-    throw new Error("All fields are mandatory !");
+  if (!name ) {
+      res.status(400);
+      throw new Error("All fields are mandatory !");
   }
   else{
-    //create new Item user
-    
+    //create new Item user      
     const Item : IItem = {
       name: name,
       user:  req?.user  as any
@@ -40,19 +36,12 @@ if(req){
     //check database if email is already taken
     await ItemService.create(Item)
     .then((newItem : IItem)=>{
-      console.log("new Item : ",newItem,typeof newItem)
-
+      console.log("new Item : ",newItem,typeof newItem);
       res.status(201).json(newItem);
     })   
-    .catch((e: any)=>{
-      errorBroadcaster(res,400,`Error:\n ${e}`)
-
-    }) 
+    .catch((error: unknown)=>{
+    exceptionHandler(error, errorBroadcaster, res);    }) 
   }
-
-}
-
-
 });
 
 
--- app/src/middlewares/validateTokenHandler.ts
+++ app/src/middlewares/validateTokenHandler.ts
@@ -4,57 +4,45 @@ const asyncHandler = require("express-async-handler");
 import * as jwt from"jsonwebtoken";
 import { IJwtPayload } from "../interfaces/IJWTPayload";
 import isJwtTokenExpired, { decode } from 'jwt-check-expiry';
+import exceptionHandler from "../utils/exceptionHandler";
+import { errorBroadcaster } from "../utils/errorBroadcaster";
 
 
 const validateToken = asyncHandler(async (req : IJwtPayload, res: Response, next: NextFunction) => {
 
   try{
-  let token;
-  let authHeader = req.headers.authorization as string;
-  console.log("authheader = ", authHeader)
+    let token;
+    let authHeader = req.headers.authorization as string;
+    console.log("authheader = ", authHeader)
   
-  if (!authHeader || !authHeader.startsWith("Bearer")|| !authHeader.split(" ")[1].trim()) {
-    console.log("authheader = if  ")
-
-    res.status(401).json("User not authorized or token missing");
-  }
-  else{
-    console.log("authheader = else  ")
-
-
-    token = authHeader.split(" ")[1];  
-    console.log('isExpired is:', isJwtTokenExpired(token));
-    if(isJwtTokenExpired(token)){
-      // res.status(401);
-      // throw new Error("token has expired");
-      console.error("token has expired")
-      res.status(401).json("token has expired");
-
+    if (!authHeader || !authHeader.startsWith("Bearer")|| !authHeader.split(" ")[1].trim()) {
+      res.status(401).json("User not authorized or token missing");
     }
+    else{
+      token = authHeader.split(" ")[1];  
+      console.log('isExpired is:', isJwtTokenExpired(token));
+      if(isJwtTokenExpired(token)){
+        console.error("token has expired")
+        res.status(401).json("token has expired");
+      }
 
-    const decoded =  jwt.verify(token, process.env.JSON_WEB_TOKEN_SECRET as jwt.Secret)
+      const decoded =  jwt.verify(token, process.env.JSON_WEB_TOKEN_SECRET as jwt.Secret)
     
-    console.log("decoded = ", decoded)
-    console.log('Decoded token :', decode(token));
-
-    const decodedPayload =  jwtDecode<IJwtPayload>(token);
-
-    const {user}  = decodedPayload
-
-    if(decoded ){
-      req.user = user
-      next();    
-
-
-    }else{
-      res.status(401);
-      throw new Error("User not authorized!");
+      console.log("decoded = ", decoded)
+      console.log('Decoded token :', decode(token));
+      const decodedPayload =  jwtDecode<IJwtPayload>(token);
+      const {user}  = decodedPayload
+      if(decoded ){
+        req.user = user
+        next();    
+      }else{
+        res.status(401);
+        throw new Error("User not authorized!");
+      }
     }
-
+  }catch(e: unknown){
+    exceptionHandler(e, errorBroadcaster, res);
   }
-}catch(e){
-  console.log(e)
-}
 });
 
 module.exports = validateToken;
