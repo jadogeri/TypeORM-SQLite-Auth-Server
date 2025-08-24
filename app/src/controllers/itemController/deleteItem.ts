23c2a841d83d519c86629148df9c2171b305d271
joseph adogeri <70236136+jadogeri@users.noreply.github.com>
2025-08-24 16:14:12 -0500

    Added JSDoc in deleteItem.ts

--- app/src/controllers/itemController/deleteItem.ts
+++ app/src/controllers/itemController/deleteItem.ts
@@ -2,7 +2,7 @@
  * @author Joseph Adogeri
  * @version 1.0
  * @since 11-AUG-2025
- *
+ * @description function to delete an item in database
  */
 
 const asyncHandler = require("express-async-handler");
@@ -10,14 +10,13 @@ import { Response } from 'express';
 import { IJwtPayload } from '../../interfaces/IJWTPayload';
 import { Item } from '../../entities/Item';
 import * as itemService from"../../services/itemService"
-import * as userService from"../../services/userService"
 import exceptionHandler from '../../utils/exceptionHandler';
 import { errorBroadcaster } from '../../utils/errorBroadcaster';
 import { User } from '../../entities/User';
 
 /**
 *@desc delete an item
-*@route DELETE /api/items/delete
+*@route DELETE /api/items
 *@access private
 */
 
@@ -28,22 +27,25 @@ export const deleteItem = asyncHandler(async (req : IJwtPayload, res: Response) 
     res.status(400);
     throw new Error(`Error: id '${stringId}' is not valid.`);    
   }
-
   const itemId = parseInt(stringId);
 
   let user: User | null= null;
-    if(req.user){
-      user = req.user as User;
-    }
+  if(req.user){
+    user = req.user as User;
+  }
+  try{
    const item : Item | null  = await itemService.getByUserId(itemId, user as User) ;
     if(!item){
       res.status(400).json("item does not exist");
     }
   
-  const deleteResult = await itemService.deleteByUserId(itemId, req)
-  if(deleteResult.affected === 1){
-    res.status(200).json({"message": `deleted item id '${itemId}' of user '${user?.username}' `});
-
+    const deleteResult = await itemService.deleteByUserId(itemId, req)
+    if(deleteResult.affected === 1){
+      res.status(200).json({"message": `deleted item id '${itemId}' of user '${user?.username}' `});
+    }
+  }catch(error: unknown){
+    exceptionHandler(error, errorBroadcaster, res) 
   }
+  
 });
 
