bf9510b30e9fa9d6a513a0403b2090aa15e64311
joseph adogeri <70236136+jadogeri@users.noreply.github.com>
2025-08-24 17:15:17 -0500

    Modified @description in forgotUser.ts

--- app/src/controllers/userController/forgotUser.ts
+++ app/src/controllers/userController/forgotUser.ts
@@ -1,8 +1,8 @@
 /**
  * @author Joseph Adogeri
  * @version 1.0
- * @since 09-FEB-2025
- *
+ * @since 24-AUG-2025
+ * @description function to begin access account;
  */
 
 import { Response, Request } from 'express';
@@ -10,14 +10,12 @@ import { IUserForgot } from '../../interfaces/IUserForgot';
 import * as userService from "../../services/userService"
 import { errorBroadcaster } from "../../utils/errorBroadcaster";
 import * as bcrypt from "bcrypt"
-import { IUser } from '../../interfaces/IUser';
 const asyncHandler = require("express-async-handler");
 import { generateRandomUUID } from '../../utils/generateRandomUUID';
 import Validator from '@josephadogeridev/auth-credential-validator-ts';
 import { User } from '../../entities/User';
 import exceptionHandler from '../../utils/exceptionHandler';
-// import { Recipient } from '../../types/Recipient';
-// import { sendEmail } from '../../tools/mail/utils/sendEmail';
+
 
 /**
 *@desc Forgot a user
@@ -38,41 +36,32 @@ export const forgotUser = asyncHandler(async (req: Request, res : Response) => {
     errorBroadcaster(res,400,"not a  valid email")
   }
 
-try{
-  const user  = await userService.getByEmail(email);
-  if (!user) {
-    errorBroadcaster(res,400,`Invalid Email ${email}`);
-  }else{
-    //generate unique password
-    const size : number = parseInt(process.env.NANOID_SIZE as string);
-    
-    // Using the alphanumeric dictionary
-    const uuid = generateRandomUUID(size)  
+  try{
+    const user  = await userService.getByEmail(email);
+    if (!user) {
+      errorBroadcaster(res,400,`Invalid Email ${email}`);
+    }else{
+      //generate unique password
+      const size : number = parseInt(process.env.NANOID_SIZE as string);
+      
+      // Using the alphanumeric dictionary
+      const uuid = generateRandomUUID(size)  
 
-    console.log("uuid === ", uuid);
-    //hash generated password
-    const hashedPassword : string = await bcrypt.hash(uuid , parseInt(process.env.BCRYPT_SALT_ROUNDS as string));
-    console.log("Hashed Password: ", hashedPassword);
-    //store generated password in database and unlock account
-    const updatedUser : User = {...user, password : hashedPassword, failedLogins : 0, isEnabled : true }
+      console.log("uuid === ", uuid);
+      //hash generated password
+      const hashedPassword : string = await bcrypt.hash(uuid , parseInt(process.env.BCRYPT_SALT_ROUNDS as string));
+      console.log("Hashed Password: ", hashedPassword);
+      //store generated password in database and unlock account
+      const updatedUser : User = {...user, password : hashedPassword, failedLogins : 0, isEnabled : true }
 
-    await userService.update(updatedUser)
-    //update user password with uuid
-    .then(()=>{
-    //   let recipient : Recipient ={
-    //     company : process.env.COMPANY as string,
-    //     username : user.username,
-    //     email : user.email,
-    //     password : uuid
-    //   }
-    //   sendEmail("forgot-password",recipient)
-    res.status(200).json({ password: uuid });
-
-   })
-  }
-     }catch(error : unknown){
-    exceptionHandler(error, errorBroadcaster, res);
-  
+      await userService.update(updatedUser)
+      //update user password with uuid
+      .then(()=>{
+        res.status(200).json({ password: uuid });
+      })
+    }
+  }catch(error : unknown){
+    exceptionHandler(error, errorBroadcaster, res);  
   }
 });
 
