a527653f0b9a7fb0b6ccf837734980d7c0071b1b
joseph adogeri <70236136+jadogeri@users.noreply.github.com>
2025-08-25 20:02:04 -0500

    Added JSDoc @description in resetUser.ts

--- app/src/controllers/userController/resetUser.ts
+++ app/src/controllers/userController/resetUser.ts
@@ -1,8 +1,8 @@
 /**
  * @author Joseph Adogeri
  * @version 1.0
- * @since 18-AUG-2025
- *
+ * @since 24-AUG-2025
+ * @description function to change user password;
  */
 
 const asyncHandler = require("express-async-handler");
@@ -11,10 +11,6 @@ import { IUserReset } from "../../interfaces/IUserReset";
 import * as userService from "../../services/userService"
 import { errorBroadcaster } from "../../utils/errorBroadcaster";
 import * as bcrypt from "bcrypt"
-// import { isValidEmail, isValidPassword } from '../../utils/inputValidation';
-import { IUser } from '../../interfaces/IUser';
-// import { sendEmail } from '../../tools/mail/utils/sendEmail';
-// import { Recipient } from '../../types/Recipient';
 import Validator from '@josephadogeridev/auth-credential-validator-ts';
 import { User } from '../../entities/User';
 import exceptionHandler from '../../utils/exceptionHandler';
@@ -28,47 +24,39 @@ import exceptionHandler from '../../utils/exceptionHandler';
 export const resetUser = asyncHandler(async (req: Request, res : Response) => {
 
   const { email, new_password, old_password } : IUserReset = req.body;
-    console.log(email,new_password,old_password)
-    if (!email || !new_password ||!old_password) {
-      res.status(400);
-      throw new Error("All fields are mandatory!");
-
-    }
-
-    const validator = new Validator("",email,new_password)
-    if(!validator.validateEmail()){
-      errorBroadcaster(res,400,"not a valid email")  
-    }
-    
-    if(!validator.validatePassword()){
-      errorBroadcaster(res,400,"not a valid new password")
-    }  
-try{
-        const user  = await userService.getByEmail(email as string);
-  if (!user) {
-    errorBroadcaster(res,400,`Invalid Email ${email}`);
-  }else{
-    if(!(await bcrypt.compare(old_password,user.password as string))){
-      errorBroadcaster(res,400,`Invalid password`);
-    }else{
-      const hashedPassword : string = await bcrypt.hash(new_password , parseInt(process.env.BCRYPT_SALT_ROUNDS as string));
-      console.log("Hashed Password: ", hashedPassword);
-      const updatedUser : User = {...user,  password : hashedPassword }
-      await userService.update(updatedUser)
-      // const recipient : Recipient = {
-      //   username : user.username,
-      //   email : user.email,
-      //   company : process.env.COMPANY
-      // }
-      // sendEmail("reset-password",recipient)
-      res.status(200).json({ message: "updated user password" });
-    }
-
+  if (!email || !new_password ||!old_password) {
+    res.status(400);
+    throw new Error("All fields are mandatory!");
   }
-  res.json({ message: "something went wrong" });
-       }catch(error : unknown){
-    exceptionHandler(error, errorBroadcaster, res);
-  
+
+  const validator = new Validator("",email,new_password)
+  if(!validator.validateEmail()){
+    errorBroadcaster(res,400,"not a valid email")  
+  }
+    
+  if(!validator.validatePassword()){
+    errorBroadcaster(res,400,"not a valid new password")
+  }  
+  try{
+    const user  = await userService.getByEmail(email as string);
+    if (!user) {
+      errorBroadcaster(res,400,`Invalid Email ${email}`);
+    }else{
+      if(!(await bcrypt.compare(old_password,user.password as string))){
+        errorBroadcaster(res,400,`Invalid password`);
+      }else{
+        const hashedPassword : string = await bcrypt.hash(new_password , parseInt(process.env.BCRYPT_SALT_ROUNDS as string));
+        console.log("Hashed Password: ", hashedPassword);
+        const updatedUser : User = {...user,  password : hashedPassword }
+        await userService.update(updatedUser)
+
+        res.status(200).json({ message: "updated user password" });
+      }
+
+    }
+    res.json({ message: "something went wrong" });
+  }catch(error : unknown){
+    exceptionHandler(error, errorBroadcaster, res);  
   }
 });
 
