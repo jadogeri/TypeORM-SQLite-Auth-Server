e473b43f4b6db6afe872134ddb34cdb73226f4a4
joseph adogeri <70236136+jadogeri@users.noreply.github.com>
2025-08-25 19:55:04 -0500

    Updated JSDoc in logoutUser.ts

--- app/src/controllers/userController/logoutUser.ts
+++ app/src/controllers/userController/logoutUser.ts
@@ -1,8 +1,8 @@
 /**
  * @author Joseph Adogeri
  * @version 1.0
- * @since 17-AUG-2025
- *
+ * @since 24-AUG-2025
+ * @description function to logout user;
  */
 
 const asyncHandler = require("express-async-handler");
@@ -10,32 +10,31 @@ import { Response, Request } from 'express';
 import { IUserAuthorized } from '../../interfaces/IUserAuthorized';
 import { errorBroadcaster } from "../../utils/errorBroadcaster";
 import * as authService from "../../services/authService"
-import { IAuth } from '../../interfaces/IAuth';
 import exceptionHandler from '../../utils/exceptionHandler';
+
 /**
 *@desc Logout a user
 *@route POST /api/users/logout
 *@access public
 */
 
-
 export const logoutUser = asyncHandler(async (req: Request, res : Response) => {
    
   const {token} :IUserAuthorized = req.body
   try{
-  if(!token){ 
-    errorBroadcaster(res,400,"field token is mandatory");
-  } 
-  const authenticatedUser = await authService.getByToken(token as string)
-  if(!authenticatedUser){
-    res.status(401).json({ message: "Already logged out" });
-  }
-  //remove auth from Auth Collection
+    if(!token){ 
+      errorBroadcaster(res,400,"field token is mandatory");
+    } 
+    const authenticatedUser = await authService.getByToken(token as string)
+    if(!authenticatedUser){
+      res.status(401).json({ message: "Already logged out" });
+    }
+    //remove auth from Auth Collection
+    
+    await authService.remove(authenticatedUser?.id as number)
   
-  await authService.remove(authenticatedUser?.id as number)
-  
-  res.status(200).json({ message: "logout the user" });
-   }catch(error : unknown){
+    res.status(200).json({ message: "logout the user" });
+  }catch(error : unknown){
     exceptionHandler(error, errorBroadcaster, res);  
   }
 });
