4a2c532d874107a50a5a78c21edc706cb4895400
joseph adogeri <70236136+jadogeri@users.noreply.github.com>
2025-08-28 00:23:34 -0500

    Implemented test cases in  Item.unit.test.ts

--- app/src/tests/unit/entities/Item.unit.test.ts
+++ app/src/tests/unit/entities/Item.unit.test.ts
@@ -1,34 +1,34 @@
 
 
 // myClass.test.ts
-import { Auth } from "../../../entities/Auth";
+import { Item } from "../../../entities/Item";
 import { User } from "../../../entities/User";
 
 
-describe('Auth ', () => {
+describe('Item ', () => {
 
   // Using an array of arrays for the data table
   test.each([
     [
         {
-            auth0 :{id:1, token: "token1"},
+            item0 :{id:1, name: "name1"},
             user0: { id : 1, username: "john smith", email:"johnsmith@gmail.com",password:"password",phone: "5041234567"}
         },
         {
-            auth0 :{id:2, token: "token2"},
+            item0 :{id:2, name: "name2"},
             user0: { id : 2, username: "jane smith", email:"janesmith@gmail.com",password:"pass",phone: "6751234567"}
         },
     ],
-  ])('create Auth instance with correct values', ({auth0, user0}) => {
+  ])('create Item instance with correct values', ({item0, user0}) => {
     // Arrange
     const user = new User(user0.id, user0.username, user0.email, user0.password, user0.phone);
 
     // Act
-    const auth = new Auth(auth0.id,user,auth0.token);
+    const item = new Item(item0.id,user,item0.name);
 
     // Assert
-    expect(auth).toBeDefined();
-    expect(auth.id).toBe(1)
+    expect(item).toBeDefined();
+    expect(item.id).toBe(1)
 
   });
 
