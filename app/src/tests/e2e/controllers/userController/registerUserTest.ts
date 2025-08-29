cabf805549c5be8a63453fc45f236108998bae91
joseph adogeri <70236136+jadogeri@users.noreply.github.com>
2025-08-29 16:26:51 -0500

    added test suite

--- app/src/tests/e2e/controllers/userController/registerUserTest.ts
+++ app/src/tests/e2e/controllers/userController/registerUserTest.ts
@@ -50,6 +50,49 @@ export const registerUserTest = () => {
   },30000)
 
 
+    test('registers user should handle duplicate accounts gracefully', async () => {
+
+    try{
+
+      console.log(__dirname)
+      let path  : string = __dirname + "/../../../__mocks__/user.json"
+      let initUser = fileReader(path)
+      console.log("init user ========================",initUser)
+      localStorage.setItem("user",initUser);      
+
+      let mock = localStorage.getItem("user");
+      console.log("mock", JSON.stringify(mock, null,4))
+      let mockObj = JSON.parse(mock as string)
+      console.log("mock obj stringify", JSON.stringify(mockObj, null,4))
+            console.log("mock obj regular", mock)
+
+      let mockUser : IUser = {
+        username: mockObj?.username as string,
+        email: mockObj.email as string,
+        password: mockObj?.password as string,
+        phone: mockObj.phone as string,
+
+        
+      }
+      const res = await request(BASE_URL).post('/users/register').send(mockUser)    
+
+      console.log("data retrieved from test == ",JSON.stringify(res, null, 2
+
+      ))
+      // if(res.statusCode ===201){
+      //   let updatedCreds = {...mockObj,... res.body,password : mockObj.password}   
+      //   localStorage.setItem("user",JSON.stringify(updatedCreds, null, 2))
+
+      // }
+
+      expect(res.statusCode).toEqual(500);
+    }catch(e){
+      console.log(e);
+    }    
+ 
+  },30000)
+
+
   
 }
 
