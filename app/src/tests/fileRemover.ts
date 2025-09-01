63a8fc6e093cbc6f80cc74dfef925acc33c39e90
joseph adogeri <70236136+jadogeri@users.noreply.github.com>
2025-09-01 00:05:12 -0500

    comment logs

--- app/src/tests/fileRemover.ts
+++ app/src/tests/fileRemover.ts
@@ -3,9 +3,9 @@ import * as fs from 'fs';
 export function fileRemover(filePath: string) {
 fs.unlink(filePath, (err) => {
     if (err) {
-        console.error(`Error deleting file: ${err}`);
+       // console.error(`Error deleting file: ${err}`);
         return;
     }
-    console.log(`${filePath} was deleted successfully.`);
+    //console.log(`${filePath} was deleted successfully.`);
 });
 }
